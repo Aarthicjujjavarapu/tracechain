147dd21080afdda91aaeab2a84853b6a290895b8
Aarthi <aarthij029@gmail.com>
2026-05-20 20:36:45 -0500

    chore: untrack auto-generated docs/.docusaurus cache

--- docs/.docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("C:\\Users\\aarth\\Downloads\\TraceChain\\docs\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("C:\\Users\\aarth\\Downloads\\TraceChain\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("C:\\Users\\aarth\\Downloads\\TraceChain\\docs\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("C:\\Users\\aarth\\Downloads\\TraceChain\\docs\\src\\css\\custom.css"),
-];
